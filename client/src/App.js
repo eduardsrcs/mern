df59be63ab18c1bb4dc570c36146711a4fa3e248
eduardsz <eduards.zvaigzne@gmail.com>
2020-12-22 15:43:40 +0200

    implemented routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import 'materialize-css'
+import { useRoutes } from './routes';
 
 function App() {
+  const routes = useRoutes()
   return (
     <div className="container">
       <h1>Hello client!!!</h1>
